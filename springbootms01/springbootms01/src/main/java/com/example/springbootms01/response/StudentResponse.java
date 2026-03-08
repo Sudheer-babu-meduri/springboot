a18568303ddbd4b734548850538710d53636d608
Sudheer-babu-meduri <sudheerchowdary156@gmail.com>
2026-03-08 09:49:35 +0530

    Update StudentResponse.java

--- springbootms01/springbootms01/src/main/java/com/example/springbootms01/response/StudentResponse.java
+++ springbootms01/springbootms01/src/main/java/com/example/springbootms01/response/StudentResponse.java
@@ -1,18 +1,17 @@
 package com.example.springbootms01.response;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class StudentResponse {
 
-		Integer sid;
-		String fname;
-		String lname;
-		int age;
-		String email;
-		
-		Address address;
-		
-	}
+    Integer sid;
+    String fname;
+    String lname;
+    int age;
+    String email;
 
-
+    List<Address> addresses;
+}
