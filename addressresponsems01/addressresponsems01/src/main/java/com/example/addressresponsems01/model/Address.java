5fb472805d01d3bae43e32c1a1684e6edbca19aa
Sudheer-babu-meduri <sudheerchowdary156@gmail.com>
2026-03-08 09:28:13 +0530

    Update Address.java
    update

--- addressresponsems01/addressresponsems01/src/main/java/com/example/addressresponsems01/model/Address.java
+++ addressresponsems01/addressresponsems01/src/main/java/com/example/addressresponsems01/model/Address.java
@@ -1,25 +1,36 @@
 package com.example.addressresponsems01.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Entity
 @Table(name = "address1")
-@Data 
+@Data
 public class Address {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
-	Integer id;
-	Integer fno;
-	String street;
-	String city;
-	String state;
-	long pincode;
-	
-	Integer student_id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)	
+    private Integer id;
+
+    @NotNull(message = "Flat number cannot be null")
+    private Integer fno;
+
+    @NotBlank(message = "Street cannot be empty")
+    @Size(min = 3, max = 50, message = "Street must be between 3 and 50 characters")
+    private String street;
+
+    @NotBlank(message = "City cannot be empty")
+    private String city;
+
+    @NotBlank(message = "State cannot be empty")
+    private String state;
+
+    @NotNull(message = "Pincode is required")
+    @Digits(integer = 6, fraction = 0, message = "Pincode must be 6 digits")
+    private Long pincode;
+
+    @NotNull(message = "Student Id cannot be null")
+    @Column(name = "student_id")
+    private Integer studentId;
 }
