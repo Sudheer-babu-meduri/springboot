5aa43d9e6ffbac02ab4cd483842c2e85ff35573d
Sudheer-babu-meduri <sudheerchowdary156@gmail.com>
2026-04-20 10:23:59 +0530

    Delete thymeleaf01/thymeleaf01 directory

--- thymeleaf01/thymeleaf01/src/main/java/com/thymeleaf/thymeleaf01/repo/CarRepo.java
+++
@@ -1,9 +0,0 @@
-package com.thymeleaf.thymeleaf01.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.thymeleaf.thymeleaf01.model.Car;
-
-public interface CarRepo extends JpaRepository<Car, Long> {
-
-}
