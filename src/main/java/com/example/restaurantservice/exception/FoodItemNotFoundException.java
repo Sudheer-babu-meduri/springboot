ae4a95e21fa49c39bcff9f64f2a4d1100aa6ee6c
Sudheer-babu-meduri <sudheerchowdary156@gmail.com>
2026-04-20 10:24:20 +0530

    Delete src directory

--- src/main/java/com/example/restaurantservice/exception/FoodItemNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.restaurantservice.exception;
-
-
-public class FoodItemNotFoundException extends RuntimeException {
-    public FoodItemNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/restaurantservice/exception/RestaurantNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.restaurantservice.exception;
-
-
-public class RestaurantNotFoundException extends RuntimeException {
-    public RestaurantNotFoundException(String message) {
-        super(message);
-    }
-}
