8b3e970c1afb4293caac6105b6e91137b98a7bac
Sudheer-babu-meduri <sudheerchowdary156@gmail.com>
2026-04-21 10:04:59 +0530

    Delete uservalidation/uservalidation directory
    delete

--- uservalidation/uservalidation/src/main/java/com/example/uservalidation/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.uservalidation.service;
-
- import com.example.uservalidation.dto.UserRequest;
-import com.example.uservalidation.entity.User;
-
-public interface UserService {
-    User registerUser(UserRequest request);
-}
-
